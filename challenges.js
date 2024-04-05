d6208d8ebf2a45744063459a1c15285ff2ae5298
Marie Blakely <marieblakely@myyahoo.com>
2024-04-05 09:23:19 -0400

    solve challenge 2

--- challenges.js
+++ challenges.js
@@ -58,7 +58,10 @@ addTwoNumbers('Hello', 5) //=> NaN
 -----------------------------------------------------------------------------*/
 // Your solution for 02-addTwoNumbers here:
 
-
+function addTwoNumbers(num1, num2){
+  if (isNaN(num1) || isNaN(num2)) return NaN
+  return num1 + num2
+}
 
 
 
